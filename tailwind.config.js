411c58701dd7099c2fee5cb862d713a2f5bd9a28
Antonio Zurano Blazquez <azurano@outlook.es>
2023-05-01 16:52:01 +0200

    insertar font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        anton: "'anton', serif",
+        inter: "'inter', serif",
+      }
+    },
   },
   plugins: [],
 }
