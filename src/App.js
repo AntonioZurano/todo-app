a3ea8af71450911f086bf6646aec1e31d24a00b1
Antonio Zurano Blazquez <azurano@outlook.es>
2023-05-02 17:37:49 +0200

    Creo array de elementos Todo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
+import { useState } from "react";
 import { Title } from "./components/Title";
 import { TodoInput } from "./components/TodoInput";
 import { TodoList } from "./components/TodoList";
 import { Todo } from "./components/Todo";
 
 function App() {
+
+  const [todos, setTodos] = useState ([
+    {
+      id:1,
+      title: 'Ver la próxima película de Marvel',
+      completed: false,
+    },
+    {
+      id:2,
+      title: 'Grabar el siguiente video',
+      completed: false,
+    },
+    {
+      id:3,
+      title: 'Lavar la ropa',
+      completed: false,
+    },
+    {
+      id:4,
+      title: 'Estudiar 2 Horas mínimo',
+      completed: false,
+    },          
+  ])
+
   return (
     <div className="bg-gray-900 min-h-screen font-inter h-full text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
         <Title />
         <TodoInput />
         <TodoList>
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
-          <Todo />
+
         </TodoList>
       </div>
     </div>
