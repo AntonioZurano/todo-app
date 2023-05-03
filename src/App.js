c025d6b3e80de49207a168676e4824b781c12394
Antonio Zurano Blazquez <azurano@outlook.es>
2023-05-03 19:48:39 +0200

    Todo ok

--- src/App.js
+++ src/App.js
@@ -54,6 +54,8 @@ function App() {
     setTodos(updateList)
   }
 
+  
+
   return (
     <div className="bg-gray-900 min-h-screen font-inter h-full text-gray-100 flex items-center justify-center py-20 px-5">
       <div className="container flex flex-col max-w-xl">
